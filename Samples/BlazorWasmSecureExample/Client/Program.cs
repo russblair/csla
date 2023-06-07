1707d0c0ee9da00b892edb8ed1849ddc1b2af59f
Russ Blair <rblair@carus.ca>
2023-06-07 15:47:19 -0400

    Created initial projects

---
+++ Samples/BlazorWasmSecureExample/Client/Program.cs
@@ -0,0 +1,32 @@
+using BlazorWasmSecureExample.Client;
+using BlazorWasmSecureExample.Client.Security;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("BlazorWasmSecureExample.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorWasmSecureExample.ServerAPI"));
+
+//builder.Services.AddApiAuthorization();
+
+builder.Services.AddScoped<AuthenticationStateProvider, TestAuthStateProvider>();
+
+//// configure CSLA Authentication and authorization
+builder.Services.AddAuthorizationCore();
+builder.Services.AddOptions();
+
+//builder.Services.AddCsla(o => o
+//  .AddBlazorWebAssembly()
+//  .DataPortal(dpo => dpo
+//    .EnableSecurityPrincipalFlowFromClient()
+//    .UseHttpProxy(options => options.DataPortalUrl = "/api/DataPortal")));
+
+await builder.Build().RunAsync();
---
+++ Samples/BlazorWasmSecureExample/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazorWasmSecureExample.Server.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BlazorWasmSecureExample.Server.Data
+{
+  public class ApplicationDbContext : DbContext  //ApiAuthorizationDbContext<ApplicationUser>
+  {
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+        //DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options)  //, operationalStoreOptions)
+    {
+    }
+  }
+}
